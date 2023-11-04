b0aaa1543561798a2c92d1103509cf4eb61e0fac
shubham singh <shubs.code@gmail.com>
2023-11-04 12:30:02 +0530

    app bar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import ResponsiveAppBar from "./_component/dashboard/ResponsiveAppBar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen container py-12 px-8">
-      <h2 className="text-center text-lg">MCQ Analytics</h2>
-    </main>
+    <>
+      <ResponsiveAppBar/>
+      {/* // <main className="min-h-screen container py-12 px-8">
+      //   <h2 className="text-center text-lg">MCQ Analytics</h2>
+      // </main> */}
+    </>
+    
+  
   )
 }
--- app/test.js
+++
@@ -1,1 +0,0 @@
-const testString = "Hello world"
