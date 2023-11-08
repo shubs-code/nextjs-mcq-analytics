8bdf2a359b5e344f0fcf0186f20dec5f65ea650e
shubham singh <shubs.code@gmail.com>
2023-11-08 15:33:26 +0530

    file input button and fast action button

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,14 +1,31 @@
 "use client"
 import { useState } from "react";
-// import default react-pdf entry
 import { Document, Page, pdfjs } from "react-pdf";
-// import pdf worker as a url, see `next.config.js` and `pdf-worker.js`
 import workerSrc from "../../pdf-worker";
+
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
+
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 
-export default function PDFViewer() {
-  const [file, setFile] = useState("");
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+export default function PDFViewer({inputfile}) {
+  const [file, setFile] = useState(inputfile??"");
   const [numPages, setNumPages] = useState(null);
 
   function onFileChange(event) {
@@ -17,16 +34,18 @@ export default function PDFViewer() {
 
   function onDocumentLoadSuccess({ numPages: nextNumPages }) {
     setNumPages(nextNumPages);
-  } 
+  }
 
   return (
-    <div>
+    <div className="">
       {
         !file && (
-          <div>
-            <label htmlFor="file">Load from file:</label>{" "}
-            <input onChange={onFileChange} type="file" />
-        </div>
+          <div className="min-h-screen flex items-center justify-center">
+            <Button component="label" variant="outlined" >
+              Open File
+              <VisuallyHiddenInput type="file" onChange={onFileChange} />
+            </Button>
+          </div>
         )
       }
       <div>
@@ -46,6 +65,11 @@ export default function PDFViewer() {
           )
         }
  
+      </div>
+      <div className="fixed bottom-6 right-6">
+        <Fab color="secondry" aria-label="edit">
+          <EditIcon />
+        </Fab>
       </div>
     </div>
   );
